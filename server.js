0234d046fcefadd688da76adb910e9851e28c42a
Nayan <nayanshingare93@gmail.com>
2021-06-15 15:41:44 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const { v4: uuidv4 } = require("uuid");
+const io = require("socket.io")(server);
+// Peer
+
+const { ExpressPeerServer } = require("peer");
+
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+});
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use("/peerjs", peerServer);
+
+app.get("/meeting", (req, rsp) => {
+  rsp.redirect(`/${uuidv4()}`);
+});
+
+app.get("/waiting-room", (req, rsp) => {
+  rsp.render("waitingroom");
+  
+});
+
+
+app.get("/", (req, rsp) => {
+  rsp.render("homerandom");
+});
+
+
+app.get("/:room", (req, res) => {
+  
+   res.render("room", { roomId: req.params.room });
+});
+
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, userId) => {
+    socket.join(roomId);
+    socket.to(roomId).emit("user-connected", userId);
+    console.log(userId)
+
+    socket.on("message", (message) => {
+      io.to(roomId).emit("createMessage", message);
+    });
+  });
+});
+
+server.listen(process.env.PORT || 3030);
